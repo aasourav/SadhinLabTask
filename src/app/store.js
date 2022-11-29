3709af882efdc80ddfb558495779e8e5d117268a
Ahsan Amin <ahsan.amin.eu181400095@gmail.com>
2022-11-29 09:03:55 +0600

    default counter removed

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import employeeSlice from '../features/employee/employeeSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     employee: employeeSlice.reducer,
   },
 });
